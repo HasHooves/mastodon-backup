275402dc52ec9ff3299db091881a32458bd8c05c
HasHooves <hashooves@gmail.com>
2019-11-29 10:38:06 -0800

    Added elasticsearch config

--- backup.sh
+++ backup.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 
-## This script requires that 'aws configure' has already been completed for the 'root' user.
-
 #Stopping mastodon processes
-systemctl stop mastodon-*
+    systemctl stop mastodon-*
 
 #Generating a database dump backup
-su - mastodon -c "cd /home/mastodon/live && pg_dump -Fc mastodon_production > backup.dump"
+    su - mastodon -c "cd /home/mastodon/live && pg_dump -Fc mastodon_production > backup.dump"
 
 #Moving the database backup
-aws s3 mv /home/mastodon/live/backup.dump s3://<BUCKET NAME>/home/mastodon/live/backup.dump
+    aws s3 mv /home/mastodon/live/backup.dump s3://<BUCKET NAME>/mastodon-backup/home/mastodon/live/backup.dump
 #Copying important files
-aws s3 cp /home/mastodon/live/.env.production s3://<BUCKET NAME>/home/mastodon/live/.env.production
-aws s3 cp /var/lib/redis/dump.rdb s3://<BUCKET NAME>/var/lib/redis/dump.rdb
-aws s3 cp /etc/nginx/sites-available/ s3://<BUCKET NAME>/etc/nginx/sites-available/ --recursive
-aws s3 cp /opt/backup s3://<BUCKET NAME>/opt/backup --recursive
+    aws s3 cp /home/mastodon/live/.env.production s3://<BUCKET NAME>/mastodon-backup/home/mastodon/live/.env.production
+    aws s3 cp /var/lib/redis/dump.rdb s3://<BUCKET NAME>/mastodon-backup/var/lib/redis/dump.rdb
+    aws s3 cp /etc/nginx/sites-available/ s3://<BUCKET NAME>/mastodon-backup/etc/nginx/sites-available/ --recursive
+    aws s3 cp /etc/elasticsearch/jvm.options s3://<BUCKET NAME>/mastodon-backup/etc/elasticsearch/jvm.options
 
 #Starting the mastodon processes
-systemctl start mastodon-web mastodon-sidekiq mastodon-streaming
+    systemctl start mastodon-web mastodon-sidekiq mastodon-streaming
